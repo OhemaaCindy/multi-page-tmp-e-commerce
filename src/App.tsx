63b074283c4acdda60463d6e1584dcd5f81d0df0
OhemaaCindy <cindyessuman67@gmail.com>
2025-06-16 15:08:07 +0000

    setup react-router-dom and added toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 function App() {
+  const router = createBrowserRouter([
+    // { path: "/", Component: LoginPage },
+    // { path: "/register", element: <RegisterPage /> },
+    // { path: "/profile", element: <ProfilePage /> },
+    // { path: "/forgot-password", element: <ForgotPassword /> },
+    // { path: "/reset-password", element: <ResetPassword /> },
+    // {
+    //   path: "/dashboard",
+    //   element: <Protectedlayout />,
+    //   children: [
+    //     {
+    //       index: true,
+    //       element: (
+    //         <ErrorBoundary fallback={<div>Something went wrong</div>}>
+    //           <React.Suspense fallback="loading....">
+    //             <HomePage />
+    //           </React.Suspense>
+    //         </ErrorBoundary>
+    //       ),
+    //     },
+    //     { path: ":id", element: <SinglePage /> },
+    //     { path: "*", element: <DashboardNotFound /> },
+    //   ],
+    // },
+    // { path: "/*", element: <NotFoundPage /> },
+  ]);
+
   return (
     <>
-      <h1 className="flex flex-center font-bold text-red-500">Hello world</h1>
+      <RouterProvider router={router} />
+      <Toaster position="top-center" reverseOrder={false} />
     </>
   );
 }
